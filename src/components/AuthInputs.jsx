84935805f589f44ca1652a44c5dba284d9eba5ee
Vadim Kim <vkim0203@gmail.com>
2024-12-24 21:35:10 +0900

    conditional styling for the email label

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -24,7 +24,7 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <div className="controls">
         <p>
-          <label>Email</label>
+          <label className={`label ${emailNotValid ? 'invalid' : undefined}`}>Email</label>
           <input
             type="email"
             className={emailNotValid ? 'invalid' : undefined}
