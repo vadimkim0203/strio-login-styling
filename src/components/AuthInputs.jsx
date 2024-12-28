35c1d981d276a6203223078cca107fdc2979d667
Vadim Kim <vkim0203@gmail.com>
2024-12-28 21:28:01 +0900

    Nested rules and media queries added to the button and header

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -24,9 +24,24 @@ const Input = styled.input`
   line-height: 1.5;
   background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
   color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
-  border: 1px solid transparent;
+  border: 1px solid ${({ $invalid }) => ($invalid ? "#f73f3f" : "transparent")};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #fe6600;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
 `;
 
 export default function AuthInputs() {
@@ -75,9 +90,9 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
+        <Button onClick={handleLogin}>
           Sign In
-        </button>
+        </Button>
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,131 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+import { styled } from "styled-components";
+
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  header img {
+    object-fit: contain;
+    margin-bottom: 2rem;
+    width: 11rem;
+    height: 11rem;
+  }
+
+  & h1 {
+    font-size: 1.5rem;
+    font-weight: 600;
+    letter-spacing: 0.4em;
+    text-align: center;
+    text-transform: uppercase;
+    color: #9a3412;
+    font-family: "Helvetica Neue";
+    margin: 0;
+  }
+
+  & p {
+    text-align: center;
+    color: #a39191;
+    margin: 0;
+  }
+
+  @media (min-width: 768px) {
+    margin-bottom: 4rem;
+
+    & h1 {
+      font-size: 2.25rem;
+    }
+  }
+
+  #auth-inputs {
+    width: 100%;
+    max-width: 28rem;
+    padding: 2rem;
+    margin: 0 auto;
+    border-radius: 0.5rem;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+    background: linear-gradient(180deg, #474232 0%, #28271c 100%);
+    color: white;
+  }
+
+  .controls {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    margin-bottom: 1.5rem;
+  }
+
+  .controls label {
+    display: block;
+    margin-bottom: 0.5rem;
+    font-size: 0.75rem;
+    font-weight: 700;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+    color: #6b7280;
+  }
+
+  .controls input {
+    width: 100%;
+    padding: 0.75rem 1rem;
+    line-height: 1.5;
+    background-color: #d1d5db;
+    color: #374151;
+    border: 1px solid transparent;
+    border-radius: 0.25rem;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  }
+
+  label.invalid {
+    color: #f87171;
+  }
+
+  input.invalid {
+    color: #ef4444;
+    border-color: #f73f3f;
+    background-color: #fed2d2;
+  }
+
+  .actions {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1rem;
+  }
+
+  .button {
+    padding: 1rem 2rem;
+    font-weight: 600;
+    text-transform: uppercase;
+    border-radius: 0.25rem;
+    color: #1f2937;
+    background-color: #fe6600;
+    border-radius: 6px;
+    border: none;
+  }
+
+  .button:hover {
+    background-color: #f0920e;
+  }
+
+  .text-button {
+    color: #fe6600;
+    border: none;
+  }
+
+  .text-button:hover {
+    color: #f0920e;
+  }
+`;
 
 export default function Header() {
   return (
-    <header>
+    <StyledHeader>
       <img src={logo} alt="A canvas" />
       <h1>Strio</h1>
       <p>A community of artists and art-lovers.</p>
-    </header>
+    </StyledHeader>
   );
 }
